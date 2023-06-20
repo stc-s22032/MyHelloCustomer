6fc535ec0caa9ed7ce01068dc4181dc616239c9b
stc-s22032 <s22032@suntech.jp>
2023-06-20 10:07:42 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/s22032/myhellocustomer/MainActivity.java
@@ -0,0 +1,57 @@
+package jp.suntech.s22032.myhellocustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ボタン・オブジェクトの用意
+        Button btClick = findViewById(R.id.btClick);
+        Button btrClear = findViewById(R.id.btrClear);
+        Button btnClear = findViewById(R.id.btnClear);
+
+
+        //リスナ・オブジェクトの用意
+        HelloCustomeListener Listener = new HelloCustomeListener();
+        //ボタンにリスナを設定
+        btClick.setOnClickListener(Listener);
+        btrClear.setOnClickListener(Listener);
+        btnClear.setOnClickListener(Listener);
+    }
+    private class HelloCustomeListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            EditText r_input =findViewById(R.id.etResi);
+            EditText n_input =findViewById(R.id.etName);
+            TextView r_output =findViewById(R.id.tvrOutput);
+            TextView n_output =findViewById(R.id.tvnOutput);
+
+            int id = v.getId();
+
+            if (id==R.id.btClick) {
+                String r_inputStr = r_input.getText().toString();
+                String n_inputStr = n_input.getText().toString();
+
+                r_output.setText(r_inputStr + "にお住いの");
+                n_output.setText(n_inputStr + "さん、こんにちは!");
+            } else if (id==R.id.btrClear) {
+                r_input.setText("");
+                r_output.setText("");
+            }else{
+                n_input.setText("");
+                n_output.setText("");
+            }
+        }
+    }
+}
